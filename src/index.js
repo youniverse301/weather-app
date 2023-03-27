e853face24eb9ed7571548e0abc2d6c0904d11b3
Bjorn <bjorneng3@gmail.com>
2023-03-26 22:01:02 -0400

    Bottom forecast added and right placeholder info added

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+const { cond } = require("lodash");
+
 const locationQuery = document.getElementById('locationQ');
 const submit = document.getElementById('submit');
 submit.addEventListener('click', getQuery);
@@ -7,8 +9,6 @@ let lastQuery = "London";
 function getQuery() {
   event.preventDefault();
   let locationValue = locationQuery.value;
-  const temperature = document.getElementById('temperature');
-  temperature.classList.add("farenheit"); // Add "farenheit" class to temperature element
   getWeather(locationValue);
   locationQuery.value = "";
 }
@@ -16,8 +16,9 @@ function getQuery() {
 async function getWeather(input) {
   try {
     const data = await fetchData(input);
-    const { location, condition, temperatureF, temperatureC } = extractData(data);
-    displayData(location, condition, temperatureF, temperatureC);
+    console.log(data);
+    const { location, condition, temperatureF, temperatureC, forecast} = extractData(data);
+    displayData(location, condition, temperatureF, temperatureC, forecast);
     lastQuery = input;
     return data;
   } catch (error) {
@@ -27,7 +28,7 @@ async function getWeather(input) {
 }
 
 async function fetchData(input) {
-  const response = await fetch("https://api.weatherapi.com/v1/current.json?key=20f112c532fa4178a5f12253232203&q=" + queryFormatter(input));
+  const response = await fetch("https://api.weatherapi.com/v1/forecast.json?key=20f112c532fa4178a5f12253232203&days=8&q=" + queryFormatter(input));
   return response.json();
 }
 
@@ -44,11 +45,13 @@ function extractData(data) {
   const condition = data.current.condition.text;
   const temperatureF = data.current.temp_f;
   const temperatureC = data.current.temp_c;
-  return { location, condition, temperatureF, temperatureC };
+  const forecast = data.forecast.forecastday;
+  return { location, condition, temperatureF, temperatureC, forecast };
 }
 
-function displayData(location, condition, temperatureF, temperatureC) {
+function displayData(location, condition, temperatureF, temperatureC, forecast) {
   new createPage(location, condition, temperatureF, temperatureC);
+  new createBottom(forecast);
   const searchError = document.getElementById('searchError');
   searchError.style.display = "none";
 }
@@ -59,34 +62,86 @@ function createPage(location, condition, temperatureF, temperatureC) {
   
   const conditionDiv = document.getElementById('condition');
   conditionDiv.innerHTML = condition;
+  temperatureImg = document.getElementById('temperatureImg');
+  temperatureImg.src = weatherIcon(condition);
   
   let currentUnit = "farenheit";
-  let currentTemp = temperatureF;
+  let currentTemp = temperatureF + " °F";
   
   const temperature = document.getElementById('temperature');
-  temperature.innerHTML = checkUnit(currentTemp, currentUnit);
+  temperature.innerHTML = currentTemp;
   
   const changeUnit = document.getElementById('changeUnit');
-  changeUnit.addEventListener('click', function() {
-    if (currentUnit === "farenheit") {
-      currentUnit = "celsius";
-      currentTemp = temperatureC + " °C";
-    } else {
+  changeUnit.addEventListener('click', () => {
+    if (currentUnit === "celsius") {
       currentUnit = "farenheit";
       currentTemp = temperatureF + " °F";
+    } else {
+      currentUnit = "celsius";
+      currentTemp = temperatureC + " °C";
     }
     temperature.innerHTML = currentTemp;
   });
 }
 
+function createBottom(forecast) {
+  const days = document.querySelectorAll('#day');
+  const daysIcon = document.querySelectorAll('#dayIcon');
+  const dayTemperature = document.querySelectorAll('#dayTemperature')
 
-function checkUnit(temperatureF,temperatureC) {
-  const temperature = document.getElementById('temperature');
-  if (temperature.classList.contains('farenheit')) {
-    return temperatureF + " °F";
-  } else {  
-    return temperatureC + " °C";
+  for (let i = 0; i < days.length; i++) {
+    const date = forecast[i + 1].date;
+    days[i].innerHTML = getDayOfWeek(date);
   }
+
+  for (let i = 0; i < daysIcon.length; i++) {
+    const condition = forecast[i + 1].day.condition.text;
+    daysIcon[i].src = weatherIcon(condition);
+  }
+
+  for (let i = 0; i < dayTemperature.length; i++) {
+    const temperatureF = forecast[i].day.avgtemp_f;
+    const temperatureC = forecast[i].day.avgtemp_c;
+
+    let currentUnit = "farenheit";
+    let currentTemp = temperatureF + " °F";
+    dayTemperature[i].innerHTML = currentTemp;
+    
+    const changeUnit = document.getElementById('changeUnit');
+    changeUnit.addEventListener('click', () => {
+      if (currentUnit === "celsius") {
+        currentUnit = "farenheit";
+        currentTemp = temperatureF + "°F";
+      } else {
+        currentUnit = "celsius";
+        currentTemp = temperatureC + "°C";
+      }
+      dayTemperature[i].innerHTML = currentTemp;
+    });
+  }
+}
+
+function weatherIcon(condition) {
+  if (condition.includes("Cloudy") || condition.includes("cloudy")) {
+    return "./imgs/overcast.png";
+  } else if (condition === "Overcast" || condition === "Mist") {
+    return "./imgs/overcast.png";
+  } else if (condition === "Sunny") {
+    return "./imgs/sunny.png";
+  } else if (condition.includes("rain")) {
+    return "./imgs/rainy.png";
+  }  else if (condition.includes("snow")) {
+    return"./imgs/snow.png";
+  } else {
+    return"./imgs/default.png";
+  }
+}
+
+function getDayOfWeek(date) {
+  const daysOfWeek = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
+  const dateFormatted = new Date(date);
+  const dayOfWeekIndex = dateFormatted.getDay();
+  return daysOfWeek[dayOfWeekIndex];
 }
 
 function displayError() {
@@ -94,6 +149,4 @@ function displayError() {
   searchError.style.display = "block";
 }
 
-const temperatureDiv = document.getElementById('temperature');
-temperatureDiv.classList.add("farenheit");
 getWeather(lastQuery);
